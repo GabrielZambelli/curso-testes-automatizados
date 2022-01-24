caf462b264f90aab6c9ddc43d807aa5d72200c31
Gabriel Zambelli <gabriel.zambelli@venturus.org.br>
2022-01-24 10:36:57 -0300

    implementation StringManipulations
    implementation methods StringManipulations. Signed-off-by: Gabriel Zambelli <g_zambelli@hotmail.com>

--- Aula 2/atividade1.js
+++ Aula 2/atividade1.js
@@ -13,15 +13,20 @@ class StringManipulations {
      * @param  {String} subStr  substring to be matched
      * @return {String}
      */
-    findFirstMatch(subStr) {}
-
+    findFirstMatch(subStr) {
+        let matchs = this.string.toUpperCase().indexOf(subStr.toUpperCase())
+        return matchs >= 0 ? subStr : ""
+    }
 
     /**
      * Returns the last substring that matches the given string
      * @param  {String} subStr  substring to be matched
      * @return {String}
      */
-    findLastMatch(subStr) {}
+    findLastMatch(subStr) {
+        let matchs = this.string.toUpperCase().lastIndexOf(subStr.toUpperCase())
+        return matchs >= 0 ? subStr : ""
+    }
 
     /**
      * Returns the fsubstring between two given other strings
@@ -29,7 +34,16 @@ class StringManipulations {
      * @param  {String} subStr2  ending of the match
      * @return {String}
      */
-    substringBetweenMatches(subStr1, subStr2) {}
+    substringBetweenMatches(subStr1, subStr2) {
+        if (this.findFirstMatch(subStr1) != "" && this.findFirstMatch(subStr2) != "") {
+            let firstIndice = this.string.toUpperCase().indexOf(subStr1.toUpperCase())
+            let lastIndice = this.string.toUpperCase().indexOf(subStr2.toUpperCase())
+            return this.string.slice(firstIndice, lastIndice).trim()
+
+        } else {
+            return ""
+        }
+    }
 
     /**
     Given the string attribute of the class, 
@@ -40,7 +54,13 @@ class StringManipulations {
     * @return {String}
     */
     both_ends() {
-
+        if (this.string.length >= 2) {
+            let arrayString = this.string.split('')
+            let newString = arrayString[0] + arrayString[1] + arrayString[arrayString.length - 2] + arrayString[arrayString.length - 1]
+            return newString
+        } else {
+            return ""
+        }
     }
 
     /**
@@ -52,6 +72,18 @@ class StringManipulations {
     * @param  {String} str1  
     * @return {String}
     */
-    fix_start(str1) {}
+    fix_start(str1) {
+        let arrayStr = str1.split("")
+
+        let newString = arrayStr.map(function(char, indice) {
+            if (indice != 0 && char == arrayStr[0]) {
+                return "*"
+            }
+            return char
+        })
+        return newString.join('')
+    }
 
 }
+
+module.exports = StringManipulations;
